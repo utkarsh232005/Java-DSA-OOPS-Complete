bd2c0f3eb30a2e2859a66c810b7405c6b27cd678
utkarsh patrikar <utkarshpatrikar@gmail.com>
2026-01-11 20:37:09 +0530

    Add nFactorial class for recursive factorial calculation

---
+++ dsa/recursion/nFactorial.java
@@ -0,0 +1,15 @@
+
+public class nFactorial {
+
+    public static int fact(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fact(n));
+    }
+}

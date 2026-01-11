864a8e61cc3ec1e8ff0eb97a97befa2738f121cc
utkarsh patrikar <utkarshpatrikar@gmail.com>
2026-01-11 20:34:24 +0530

    Add sumOfn class for recursive sum calculation and SortedRotated class for searching in rotated sorted array

---
+++ dsa/recursion/sumOfn.java
@@ -0,0 +1,15 @@
+
+public class sumOfn {
+
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(sum(n));
+    }
+}
